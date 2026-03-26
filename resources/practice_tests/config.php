69a67e6165a24522da1cad3ee457d331bcb1bd44
Ashonibare <animasahunvictor1@gmail.com>
2026-03-26 17:32:31 +0100

    new changes

--- resources/practice_tests/config.php
+++ resources/practice_tests/config.php
@@ -7,61 +7,66 @@
 // Get single test by code
 function getTest($code) {
     global $db;
-    
+   
     $stmt = $db->prepare("
-        SELECT * FROM tests 
+        SELECT * FROM tests
         WHERE code = ? AND is_active = 1
+        LIMIT 1
     ");
     $stmt->execute([$code]);
     $test = $stmt->fetch(PDO::FETCH_ASSOC);
-    
+   
     if (!$test) return null;
-    
+   
     // Map database fields to template-friendly names
     return [
-        'code' => $test['code'],
-        'type' => str_replace('_', ' ', $test['test_type']),
-        'task' => $test['task_number'],
-        'title' => $test['title'],
-        'description' => $test['description'],
-        'instructions' => $test['instructions'],
-        'time_limit' => $test['duration_minutes'],
-        'word_target' => $test['word_target'],
-        'word_max' => $test['word_max'],
-        'media_file' => $test['media_file'],
-        'category' => $test['category']
+        'code'          => $test['code'],
+        'type'          => str_replace('_', ' ', $test['test_type']),
+        'task'          => $test['task_number'],
+        'title'         => $test['title'],
+        'description'   => $test['description'],
+        'instructions'  => $test['instructions'],
+        'time_limit'    => $test['duration_minutes'],
+        'word_target'   => $test['word_target'],
+        'word_max'      => $test['word_max'],
+        'media_file'    => $test['media_file'],
+        'category'      => $test['category'],
+        // Add these for safety (in case code uses them)
+        'test_type'     => $test['test_type'],
+        'duration_minutes' => $test['duration_minutes'],
+        'max_attempts'  => $test['max_attempts'] ?? 999,
+        'cooldown_days' => $test['cooldown_days'] ?? 90
     ];
 }
 
 // Get tests by type
 function getTestsByType($type) {
     global $db;
-    
-    // Convert "IELTS Academic Writing" to "IELTS_Academic_Writing"
+   
     $dbType = str_replace(' ', '_', $type);
-    
+   
     $stmt = $db->prepare("
-        SELECT * FROM tests 
+        SELECT * FROM tests
         WHERE test_type LIKE ? AND is_active = 1
         ORDER BY code
     ");
     $stmt->execute(["%$dbType%"]);
     $tests = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    
-    // Map each test
+   
     return array_map(function($test) {
         return [
-            'code' => $test['code'],
-            'type' => str_replace('_', ' ', $test['test_type']),
-            'task' => $test['task_number'],
-            'title' => $test['title'],
-            'description' => $test['description'],
-            'instructions' => $test['instructions'],
-            'time_limit' => $test['duration_minutes'],
-            'word_target' => $test['word_target'],
-            'word_max' => $test['word_max'],
-            'media_file' => $test['media_file'],
-            'category' => $test['category']
+            'code'          => $test['code'],
+            'type'          => str_replace('_', ' ', $test['test_type']),
+            'task'          => $test['task_number'],
+            'title'         => $test['title'],
+            'description'   => $test['description'],
+            'instructions'  => $test['instructions'],
+            'time_limit'    => $test['duration_minutes'],
+            'word_target'   => $test['word_target'],
+            'word_max'      => $test['word_max'],
+            'media_file'    => $test['media_file'],
+            'category'      => $test['category'],
+            'test_type'     => $test['test_type']
         ];
     }, $tests);
 }
